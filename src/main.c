96203a56d8d58ce9b3972effc5b48af023ad5a96
loculy <loculy@k1r4p7.42mulhouse.fr>
2023-06-21 15:09:58 +0200

    r

--- src/main.c
+++ src/main.c
@@ -16,27 +16,16 @@ int	main(int argc, char **argv)
 	t_cooldown	cooldown;
 
 	/* ========= PARSING ========= */
-	/*
+	
 	if (argc != 2)
 		return (ft_errormap("wrong number of args"));
 	if (check_ber(argv[1]) == 0)
 		return (0);
 	get_file(&main, argv[1]);
 	ft_pars_raw_map(main.raw_map);
-	*/
+	
 	/* ========= RAYCAST ========= */
-	map.map = ftm_malloc(6 * sizeof(char *));
-    map.map[0] = ft_strdup("111111\n");
-    map.map[1] = ft_strdup("100D11\n");
-    map.map[2] = ft_strdup("100ND1\n");
-    map.map[3] = ft_strdup("101001\n");
-    map.map[4] = ft_strdup("111111");
-    //map.map[0] = ft_strdup("000000\n");
-	//map.map[1] = ft_strdup("000010\n");
-	//map.map[2] = ft_strdup("000N00\n");
-	//map.map[3] = ft_strdup("000000\n");
-	//map.map[4] = ft_strdup("000000");
-    map.map[5] = 0;
+    map.map = 0;
     map = init_map(map);
 	main.map = &map;
 
